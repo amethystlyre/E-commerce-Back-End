9da2fda74deaf41e000d4398c417b307774aed91
amethystlyre <91407728+amethystlyre@users.noreply.github.com>
2023-11-18 16:48:33 +1100

    update models and relationships

--- models/index.js
+++ models/index.js
@@ -5,12 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+      model: ProductTag,
+      unique: false
+  },
+  as: 'tags'
+});
+Product.hasMany(ProductTag);
+ProductTag.belongsTo(Product);
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+      model: ProductTag,
+      unique: false
+  },
+  as: 'products'
+});
+
+Tag.hasMany(ProductTag);
+ProductTag.belongsTo(Tag);
+
 
 module.exports = {
   Product,
